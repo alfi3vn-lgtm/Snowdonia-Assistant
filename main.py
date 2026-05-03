138f38560f895db038e06e9c0cca7dd62c869954
alfi3vn-lgtm <alfi3vn@gmail.com>
2026-05-03 23:26:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -252,7 +252,7 @@ ROLE_NAME_MAP = {
     "Assistant Headteacher":         "Assistant Headteacher",
     "Deputy Headteacher":            "Deputy Headteacher",
     "Headteacher":                   "Headteacher",
-    "Executive Headteacher":         "Executive Headteacher",
+    "Chief Operating Officer":       "Chief Operating Officer",
     "Chief Education Officer":       "Chief Education Officer",
 }
 
